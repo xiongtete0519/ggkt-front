1d8fdb6b9a6796c8ef206ee58370efcbb6f7eaa5
interface_xiongtete <1144722582@qq.com>
2022-08-16 21:26:19 +0800

    课程分类-导出功能

--- src/views/vod/subject/list.vue
+++ src/views/vod/subject/list.vue
@@ -18,6 +18,12 @@
         label="创建时间">
       </el-table-column>
     </el-table>
+
+    <div class="el-toolbar">
+      <div class="el-toolbar-body" style="justify-content: flex-start;">
+        <el-button type="text" @click="exportData"><i class="fa fa-plus"/> 导出</el-button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -33,6 +39,10 @@ export default {
     this.getSubList(0)
   },
   methods: {
+    //课程分类导出
+    exportData() {
+      window.open("http://localhost:8301/admin/vod/subject/exportData")
+    },
     //数据字典列表
     getSubList(id) {
       subjectApi.getChildList(id)
