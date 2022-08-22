bcb34d6793b99f0488fb6add2ef4436f69872a98
interface_xiongtete <1144722582@qq.com>
2022-08-22 17:54:24 +0800

    订单列表前端(条件+分页)

--- src/router/index.js
+++ src/router/index.js
@@ -149,6 +149,24 @@ export const constantRoutes = [
     ]
   },
   {
+    path: '/order',
+    component: Layout,
+    redirect: '/order/list',
+    name: 'Order',
+    meta: { title: '订单管理', icon: 'el-icon-truck' },
+    alwaysShow: true,
+    children: [
+      {
+        path: 'orderInfo/list',
+        name: 'OrderInfo',
+        component: () => import('@/views/order/list'),
+        meta: { title: '订单列表' }
+      }
+    ]
+  },
+
+
+  {
     path: '/example',
     component: Layout,
     redirect: '/example/table',
